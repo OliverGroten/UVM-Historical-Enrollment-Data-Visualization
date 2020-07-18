da655cad9c63c23f121b92946e43724351f0d339
Oliver Groten <oliver.reckordgroten@gmail.com>
2020-07-18 15:02:31 -0400

    web scraper proof of concept, and initial set of data

--- python/web-scraper.py
+++ python/web-scraper.py
@@ -1,7 +1,13 @@
 import urllib.request
 
-url = "https://giraffe.uvm.edu/~rgweb/batch/enrollment/curr_enroll_199501.txt"
-response = urllib.request.urlopen(url)
+url_base = "https://giraffe.uvm.edu/~rgweb/batch/enrollment/curr_enroll_"
 
-for line in response:
-    print(line.decode('utf-8'))
+for i in range(1995, 2021):
+    for j in [1, 6, 9]:
+        url_end = str(i) + "0" + str(j) + ".txt"
+        url = url_base + url_end
+        f = open("../data/" + url_end, 'w')
+        response = urllib.request.urlopen(url)
+        for line in response:
+            f.write(line.decode('utf-8'))
+        f.close()
