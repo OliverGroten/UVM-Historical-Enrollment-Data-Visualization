49763dda742385b5d8d4cb247a07a5aaeb624677
Oliver Groten <oliver.reckordgroten@gmail.com>
2021-04-11 23:42:47 -0400

    clean up web_scraper and test database connection

--- python/web-scraper.py
+++ python/web-scraper.py
@@ -1,22 +1,37 @@
-#import urllib.request
-from sqlalchemy import create_engine
+import mysql.connector
+import urllib.request
+# URL segement to fetch data from
+url_base = "https://serval.uvm.edu/~rgweb/batch/enrollment/curr_enroll_"
 
-#url_base = "https://serval.uvm.edu/~rgweb/batch/enrollment/curr_enroll_"
+def main():
+    scrape_data()
+    db_connect()
 
-#for i in range(1995, 2021):
-#    for j in [1, 6, 9]:
-#        url_end = str(i) + "0" + str(j) + ".txt"
-#        url = url_base + url_end
-#        f = open("../data/" + url_end, 'w')
-#        response = urllib.request.urlopen(url)
-#        for line in response:
-#            f.write(line.decode('utf-8'))
-#        f.close()
 
-ssl_args = {'ssl': {'ca': '/etc/pki/tls/certs/webdb-cacert.pem'}}
-db_engine = create_engine(
-        'mysql://oreckord:74&9d$xVVWz1@webdb.uvm.edu/ORECKORD_HISTORICAL_ENROLLMENT',
-        connect_args=ssl_args)
-Session = sessionmaker(bind=db_engine)
-db = Session()
-db.table_names()
+def scrape_data():
+    for i in range(1995, 2021):
+       for j in [1, 6, 9]:
+           url_end = str(i) + "0" + str(j) + ".txt"
+           url = url_base + url_end
+           f = open("../data/" + url_end, 'w')
+           response = urllib.request.urlopen(url)
+           for line in response:
+               f.write(line.decode('utf-8'))
+           f.close()
+
+
+def db_connect():
+    mydb = mysql.connector.connect(
+    	host="webdb.uvm.edu",
+    	user="oreckord_reader",
+    	password="hfDC3mIJGXkG"
+    )
+    print(mydb)
+    cursor = mydb.cursor()
+    cursor.execute("SHOW DATABASES")
+    for x in cursor:
+    	print(x)
+
+
+if __name__== "__main__":
+  main()
